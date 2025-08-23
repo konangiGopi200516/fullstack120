ba0d215672e932d3a9b4643ad946b94dd788c994
konangiGopi200516 <gopikonangi0001@gmail.com>
2025-08-23 10:31:52 +0530

    cicd created

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://16.16.157.97:9090/springapp1';
+const BASE_URL = 'http:// 51.20.197.51:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
